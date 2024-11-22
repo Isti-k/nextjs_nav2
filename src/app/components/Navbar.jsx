0cd595282d63b0d9f82c540e801a3d3630a56773
Isti-k <kiscserkes03@gmail.com>
2024-11-22 10:48:54 +0100

    mobile view, hamburger-bar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React, { useState } from 'react';
 
 function Navbar() {
@@ -17,15 +19,51 @@ function Navbar() {
                         /* Hamburger Button For Mobile */
                         <div className='md:hidden'>
                             <button className='p-2 text-gray-700 rounded-md outline-none focus border-gray-400' onClick={() => setNavbar(!navbar)}>
-                            
+                            {navbar ? (
+                                <Image src="/6596459.png" width={30} height={30} alt="logo" />
+                            ) : (
+                                <Image
+                                    src="/hamburger-burger.svg"
+                                    width={30}
+                                    height={30}
+                                    alt="logo"
+                                    className="focus:border-none active:border-none"
+                                />
+                            )}
                             </button>
                         </div>
+                    </div>
+                </div>
+                <div>
+                    <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${ navbar ? 'p-12 md:p-0 block' : 'hidden'}`}>
+                        <ul className="h-screen md:h-auto items-center justify-center md:flex ">
+                            <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                            <Link href="#about" onClick={() => setNavbar(!navbar)}>
+                                About
+                            </Link>
+                            </li>
+                            <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                            <Link href="#blog" onClick={() => setNavbar(!navbar)}>
+                                Blogs
+                            </Link>
+                            </li>
+                            <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                            <Link href="#contact" onClick={() => setNavbar(!navbar)}>
+                                Contact
+                            </Link>
+                            </li>
+                            <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                            <Link href="#projects" onClick={() => setNavbar(!navbar)}>
+                                Projects
+                            </Link>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
         </nav>
     </div>
-  )
+  );
 }
 
 export default Navbar
